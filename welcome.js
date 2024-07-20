f0f8084a26cd4ec56ade829e2b71cba665133c5b
duckie <93015048+duckiec@users.noreply.github.com>
2024-07-20 19:08:16 -0400

    Delete welcome.js

--- welcome.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const username = sessionStorage.getItem('username');
-    if (username) {
-        document.getElementById('username').textContent = username;
-    } else {
-        window.location.href = '/';
-    }
-});
